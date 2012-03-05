3020e598ef02b07d784408519dd3079cc3170ee4
Zernel <itzernel@gmail.com>
2012-03-05 19:34:55 +0800

    Add actions of CRUD

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -15,10 +15,12 @@ class PartiesController < ApplicationController
 
     respond_to do |format|
       if @party.save
+        binding.pry
         format.html { redirect_to @party, notice: '成功发起聚会' }
         format.json { render json: @party, status: :created, location: @party }
       else
-        format.html { render aciton: 'new'}
+        binding.pry
+        format.html { render aciton: 'new' }
         format.json { render json: @party.errors, status: :unprocessable_entity }
       end
     end
@@ -30,6 +32,34 @@ class PartiesController < ApplicationController
     respond_to do |format|
       format.html
       format.json { render json: @party }
+    end
+  end
+
+  def edit
+    @party = Party.find(params[:id])
+  end
+
+  def update
+    @party = Party.find(params[:id])
+
+    respond_to do |format|
+      if @party.update_attributes(params[:party])
+        format.html { redirect_to @party, notice: '成功更新聚会信息' }
+        format.json { head :no_content }
+      else
+        format.html { render aciton: 'edit'}
+        format.json { render json: @party.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @party = Party.find(params[:id])
+    @party.destroy
+
+    respond_to do |format|
+        format.html { redirect_to parties_url }
+        format.json { head :no_content }
     end
   end
 end

c51ded16e906ddfd32db9ef038e4bacf3a685dc2
Zernel <itzernel@gmail.com>
2012-04-22 16:10:59 +0800

    Updated the seed

--- app/models/party.rb
+++ app/models/party.rb
@@ -1,13 +1,21 @@
 class Party
   include Mongoid::Document
 
+  paginates_per 5
+
   field :topic, :type => String
+  field :location, :type => String
   field :presenter, :type => String
   field :date, :type => String
   field :address, :type => String
   field :introduction
 
+  validates :location, presence: true
   validates :topic, presence: true
   validates :date, presence: true
   validates :address, presence: true
+
+  def title
+    "[#{location}][#{date}] #{topic}"
+  end
 end
